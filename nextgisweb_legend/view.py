c7acb48ef8225d499e2cd9bf64a6794ed1016066
Шиниев Павел <shpawel@mail.ru>
2019-05-24 14:34:34 +0300

    ---

--- nextgisweb_legend/view.py
+++ nextgisweb_legend/view.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+from __future__ import unicode_literals, absolute_import, print_function
 import nextgisweb.dynmenu as dm
 from nextgisweb.resource import Widget, Resource
 
