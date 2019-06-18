5d0e302b2cf06976ea1bee6b3ff97cfff09f85e3
Шиниев Павел <shpawel@mail.ru>
2019-06-18 11:25:15 +0300

    добавлена автогенерация id объетка внутри легенды и назначение идентификатора легенды

--- nextgisweb_legend/api.py
+++ nextgisweb_legend/api.py
@@ -1,15 +1,50 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+from __future__ import print_function
 
-from pyramid.response import FileResponse
+from json import dumps, loads
+from uuid import uuid4
+
+from pyramid.response import FileResponse, Response
+from pyramid.httpexceptions import HTTPBadRequest
 
 from nextgisweb.env import env
-from nextgisweb.resource import resource_factory, ResourceScope
+from nextgisweb.resource import resource_factory, ResourceScope, Resource
+from nextgisweb.models import DBSession
+
 
 from .model import LegendSprite
 
+
 def legend(request):
-    pass
+    if 'styles' not in request.GET.keys():
+        raise HTTPBadRequest("Parameter 'styles' not found.")
+    try:
+        styles = map(int, request.GET.getall('styles'))
+    except ValueError:
+        raise HTTPBadRequest("Invalid 'styles' parameter. Only numbers.")
+
+    result = []
+
+    legend_list = DBSession.query(Resource).filter(Resource.parent_id.in_(styles))
+
+    for legend in legend_list:
+        legend_description = env.file_storage.filename(legend.description_fileobj)
+        with open(legend_description, mode='r') as f:
+            description = loads(f.read(), encoding='utf-8')
+            if type(description) != list:
+                description = list(description)
+            element = dict(
+                id=legend.id,
+                legend_id=legend.id,
+                name=legend.display_name or legend.keyname,
+                children=description
+            )
+            result.append(element)
+
+    return Response(
+        dumps(result),
+        content_type='application/json'
+    )
 
 
 def description_file(request):
@@ -23,6 +58,7 @@ def description_file(request):
 
     return response
 
+
 def image_file(request):
     request.resource_permission(ResourceScope.read)
 
@@ -34,18 +70,18 @@ def image_file(request):
 
     return response
 
+
 def setup_pyramid(comp, config):
     config.add_route(
-        'legend.legend', '/api/resource/{id}/legend',
-        factory=resource_factory
-    ).add_view(legend, context=LegendSprite, request_method='GET')
+        'legend.legend', '/api/resource/legend',
+    ).add_view(legend, request_method='GET')
 
     config.add_route(
-        'legend.description', '/api/resource/{id}/legend/description',
+        'legend.description', '/api/resource/{id:\d+}/legend/description',
         factory=resource_factory
     ).add_view(description_file, context=LegendSprite, request_method='GET')
 
     config.add_route(
-        'legend.image', '/api/resource/{id}/legend/image',
+        'legend.image', '/api/resource/{id:\d+}/legend/image',
         factory=resource_factory
     ).add_view(image_file, context=LegendSprite, request_method='GET')
--- nextgisweb_legend/model.py
+++ nextgisweb_legend/model.py
@@ -1,18 +1,37 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals, print_function, absolute_import
+from __future__ import print_function, absolute_import
 
+from json import loads, dumps
 from shutil import copyfileobj
+from uuid import uuid4
 
 from nextgisweb import db
 from nextgisweb.env import env
 from nextgisweb.file_storage import FileObj
 from nextgisweb.models import declarative_base
 from nextgisweb.render import IRenderableStyle
-from nextgisweb.resource import DataScope, Resource, ResourceScope, Serializer, SerializedProperty
+from nextgisweb.resource import DataScope, Resource, ResourceScope, Serializer, SerializedProperty, ValidationError
 
 from .util import _
 
 Base = declarative_base()
+
+
+def normalize_description(desc, legend_id):
+    """
+    Фунция нормализации идентификаторов объектов
+
+    Если такого поля нет, то она его добавит. Иначе изменит существующие значение на uuid
+
+    :param dict desc:
+    :param int legend_id: идентификатор ресурса легенды
+    :return:
+    """
+    desc['id'] = str(uuid4())
+    desc['legend_id'] = legend_id
+    desc.setdefault('children', [])
+    for child in desc['children']:
+        normalize_description(child, legend_id=legend_id)
 
 
 class LegendSprite(Base, Resource):
@@ -31,6 +50,7 @@ class LegendSprite(Base, Resource):
     def check_parent(cls, parent):
         return IRenderableStyle.providedBy(parent)
 
+
 class _description_file_attr(SerializedProperty):  # NOQA
 
     def setter(self, srlzr, value):
@@ -40,7 +60,12 @@ class _description_file_attr(SerializedProperty):  # NOQA
         dstfile = env.file_storage.filename(fileobj, makedirs=True)
 
         with open(srcfile, 'r') as fs, open(dstfile, 'w') as fd:
-            copyfileobj(fs, fd)
+            desc = loads(fs.read(), encoding='utf-8')
+            if not isinstance(desc, list):
+                raise ValidationError(_('Legend\'s description must be a list'))
+            for el in desc:
+                normalize_description(el, legend_id=srlzr.obj.id)
+            fd.write(dumps((desc)))
 
 
 class _image_file_attr(SerializedProperty):  # NOQA
@@ -51,7 +76,7 @@ class _image_file_attr(SerializedProperty):  # NOQA
         srlzr.obj.image_fileobj = fileobj
         dstfile = env.file_storage.filename(fileobj, makedirs=True)
 
-        with open(srcfile, 'r') as fs, open(dstfile, 'w') as fd:
+        with open(srcfile, 'rb') as fs, open(dstfile, 'wb') as fd:
             copyfileobj(fs, fd)
 
 
